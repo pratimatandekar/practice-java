b408c59ce68b2290b58a81419bc38ed34143a1a1
pratimatandekar <tandekarpratima9@gmail.com>
2025-04-30 22:58:53 +0530

    added

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ class Main {
     public static void main(String[] args) {
         //printArray();
        // evenArray();
-        sumOfArray();
+       // sumOfArray();
+        arrEle();
     }
 
     public static  void printArray(){
@@ -35,6 +36,23 @@ class Main {
         System.out.println("The sum of array is" +sum);
     }
 
+
+    //trying array traversing using for each loop
+    public static void arrEle(){
+          int  []arr={1,2,3,4,5};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(i);
+
+        }
+        System.out.println();
+        //  for each loop
+        for (int i :arr) {
+            System.out.println(i);
+            
+        }
     }
+
+    }
+
 
 
